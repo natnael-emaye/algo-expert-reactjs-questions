48b37de3e156a798f82428b257320775125ee469
natnael-emaye <natnaelabebegualu@gmail.com>
2023-11-17 11:46:23 +0300

    import Calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import TipCalculator from "./Questions/TipCalculator";
+
 function App() {
     return (
-        <div className="max-w-[1440px] mx-auto w-[80vw] h-screen flex flex-col  ">
-          <div></div>
+        <div className="max-w-[1440px] w-[80vw]  h-[90vh] flex flex-col mx-auto items-center justify-center ">
+          {/* <TipCalculator/> */}
         </div>
     );
 }
