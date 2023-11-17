f4092d9a60ecae9bc4236e65ae4c58ac3d9788a4
natnael-emaye <natnaelabebegualu@gmail.com>
2023-11-17 09:17:07 +0300

    initialize the project and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="max-w-[1440px] mx-auto w-[80vw] h-screen flex flex-col  ">
+          <div></div>
+        </div>
+    );
+}
+
+export default App;
