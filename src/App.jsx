1aed743da32d551b9b3271b759a9f536946ebb54
natnael-emaye <natnaelabebegualu@gmail.com>
2023-11-18 21:35:41 +0300

    import QuestionList

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable no-unused-vars */
+import QuestionList from "./Questions/QuestionList";
 import TipCalculator from "./Questions/TipCalculator";
 
 function App() {
     return (
-        <div className="max-w-[1440px] w-[80vw]  h-[90vh] flex flex-col mx-auto items-center justify-center ">
+        <div className="max-w-[1440px] w-[80vw] items-center flex pt-14 h-[90vh]  flex-col mx-auto">
           {/* <TipCalculator/> */}
+          <QuestionList/>
         </div>
     );
 }
