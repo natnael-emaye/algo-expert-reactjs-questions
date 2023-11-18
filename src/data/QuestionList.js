4573bb8b1dba54b16b76e487470402601cd33d35
natnael-emaye <natnaelabebegualu@gmail.com>
2023-11-18 21:35:02 +0300

    create moke api data

---
+++ src/data/QuestionList.js
@@ -0,0 +1,104 @@
+export const questions = [
+    {
+        id: "sign-up-form",
+        name: "sign-up-form",
+        Category: "HTML",
+    },
+    {
+        id: "item-cart",
+        name: "Item Cart",
+        Category: "HTML",
+    },
+    {
+        id: "spaghetti-recipe",
+        name: "Spaghetti Recipe",
+        Category: "HTML",
+    },
+    {
+        id: "rainbow-circle",
+        name: "Rainbow Circle",
+        Category: "CSS",
+    },
+    {
+        id: "navbar",
+        name: "Navbar",
+        Category: "CSS",
+    },
+    {
+        id: "pig-emoji",
+        name: "pig emoji",
+        Category: "CSS",
+    },
+    {
+        id: "testing-framework",
+        name: "Testing Framework",
+        Category: "Javascript",
+    },
+    {
+        id: "array",
+        name: "Array",
+        Category: "Javascript",
+    },
+    {
+        id: "big-o",
+        name: "Big-O",
+        Category: "Javascript",
+    },
+    {
+        id: "this-binding",
+        name: "This Binding",
+        Category: "Javascript",
+    },
+    {
+        id: "binary-search",
+        name: "Binary Search",
+        Category: "Javascript",
+    },
+];
+
+export const submissions = [
+    {
+        id: "sign-up-form",
+        status:"CORRECT"
+    },
+    {
+        id: "item-cart",
+        status:"INCORRECT"
+    },
+    {
+        id: "spaghetti-recipe",
+        status:"CORRECT"
+    },
+    {
+        id: "rainbow-circle",
+        status:"INCORRECT"
+    },
+    {
+        id: "navbar",
+        status:"CORRECT"
+    },
+    {
+        id: "pig-emoji",
+        status:"INCORRECT"
+    },
+    {
+        id: "testing-framework",
+        status:"CORRECT"
+    },
+    {
+        id: "array",
+        status:"INCORRECT"
+    },
+    {
+        id: "big-o",
+        status:"CORRECT"
+    },
+    {
+        id: "this-binding",
+        status:"INCORRECT"
+    },    
+    {
+        id: "binary-search",
+        status:"INCORRECT"
+    },    
+]
