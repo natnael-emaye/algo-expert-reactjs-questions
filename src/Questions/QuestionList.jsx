1c06801148001a1b5e816d609ffa9b79a23ed3ba
natnael-emaye <natnaelabebegualu@gmail.com>
2023-11-19 22:22:19 +0300

    project completed without refactor

--- src/Questions/QuestionList.jsx
+++ src/Questions/QuestionList.jsx
@@ -3,33 +3,82 @@
 import { questions, submissions } from "../data/QuestionList";
 
 export default function QuestionList() {
+    const category = new Set([]);
+    questions.forEach((value) => {
+        return category.add(value.Category);
+    });
+
     return (
         <div className="w-full h-full flex flex-wrap gap-3 justify-center">
-            <Category category="HTML" />
-            <Category category="CSS" />
-            <Category category="Javascript" />
+            {Array.from(category).map((categoryItem, index) => {
+                const { questionsByCategory, completedQuestion } =
+                    filterQuestionByCategory(categoryItem);
+                return (
+                    <Category
+                        key={index}
+                        completedQuestion={completedQuestion}
+                        questions={questionsByCategory}
+                        category={categoryItem}
+                    />
+                );
+            })}
         </div>
     );
 }
 
-function Category({ category }) {
+function Category({ category, completedQuestion, questions }) {
+    // const status =
     return (
         <div className="flex flex-col flex-1  ">
-            <h2 className="text-xl font-bold text-slate-950 mb-5">{category}</h2>
+            <h2 className="text-xl font-bold text-slate-950 mb-5">
+                {category} - {completedQuestion}/{questions.length}
+            </h2>
             <div className="flex flex-col">
-                <Question question="Ping Emoji" />
-                <Question question="Ping Emoji" />
+                {questions.map((question) => {
+                    let status = submissions.find(
+                        (submission) => submission.id === question.id
+                    );
+                    return (
+                        <Question
+                            key={question.id}
+                            status={status.status}
+                            question={question.name}
+                        />
+                    );
+                })}
             </div>
         </div>
     );
 }
 
-function Question({ question }) {
+function Question({ question, status }) {
+    const isChecked = status === "CORRECT" ? "bg-green-500" : "bg-red-500";
     return (
-        <div className="cursor-pointer my-2 hover:animate-pulse bg-white px-2 border-r-[23px] shadow-md border-emerald-700 rounded-md ">
+        <div className="flex items-center cursor-pointer my-2 hover:animate-pulse bg-white px-2 border-r-[23px] shadow-md border-emerald-700 rounded-md ">
+            <span className={"mx-1 w-6 h-6  rounded-full " + isChecked}></span>
             <h2 className="text-emerald-600 text-lg font-bold py-2">
                 {question}
             </h2>
         </div>
     );
+}
+
+function filterQuestionByCategory(categoryItem) {
+    let completedQuestion = 0;
+    const questionsByCategory = questions.filter((item) => {
+        if (item.Category == categoryItem) {
+            const findSubmissionByID = submissions.find(
+                (subitem) => subitem.id == item.id
+            );
+            if (findSubmissionByID.status == "CORRECT") {
+                completedQuestion++;
+            }
+        }
+        return item.Category == categoryItem;
+    });
+
+    return {
+        questionsByCategory,
+        completedQuestion,
+    };
 }
