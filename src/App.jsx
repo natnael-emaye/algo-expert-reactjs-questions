be890abd432d4298f8bc10dbc90ff034d6480d45
natnael-emaye <natnaelabebegualu@gmail.com>
2023-11-23 22:11:19 +0300

    import quiz challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable no-unused-vars */
 import QuestionList from "./Questions/QuestionList";
 import TipCalculator from "./Questions/TipCalculator";
+import Quiz from "./Questions/Quiz";
 
 function App() {
     return (
         <div className="max-w-[1440px] w-[80vw] items-center flex pt-14 h-[90vh]  flex-col mx-auto">
           {/* <TipCalculator/> */}
-          <QuestionList/>
+          {/* <QuestionList/> */}
+          <Quiz/>
         </div>
     );
 }
