10f67fcc5e19932bda8ae0693c48db3d0a383c42
natnael-emaye <natnaelabebegualu@gmail.com>
2023-11-23 22:13:09 +0300

    create mock quiz data

---
+++ src/Questions/Quiz.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const QUIZ_Data = [
+    {
+        question:
+            "question number 1 Which one of the following is built-in React Hook?",
+        answers: ["useState", "usefetch", "useTimeout", "useLocalStorage"],
+        correctAnswer: 0,
+    },
+    {
+        question:
+            "question number 2 Which one of the following is built-in React Hook?",
+        answers: ["useState", "usefetch", "useTimeout", "useLocalStorage"],
+        correctAnswer: 1,
+    },
+    {
+        question:
+            "Last question Which one of the following is built-in React Hook?",
+        answers: ["useState", "usefetch", "useTimeout", "useLocalStorage"],
+        correctAnswer: 2,
+    },
+];
+
+
