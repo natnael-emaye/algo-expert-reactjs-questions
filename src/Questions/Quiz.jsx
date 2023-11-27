31d0d3d145597f2665af7f86533d6fd1e1cc9394
natnael-emaye <natnaelabebegualu@gmail.com>
2023-11-27 11:12:52 +0300

    complete my app with tracking chosen answers

--- src/Questions/Quiz.jsx
+++ src/Questions/Quiz.jsx
@@ -23,45 +23,68 @@ const QUIZ_Data = [
 
 export default function Quiz() {
     const [quizIndex, setQuizIndex] = useState(0);
-    const [selectedIndex, setSelector] = useState(-1);
+    const [chooseAnswers, setChooseAnswers] = useState([]);
 
     const quiz = QUIZ_Data[quizIndex];
 
+    function updateChosenAnswer(quizIndex, answerIndex) {
+        const newChooseAnswers = [...chooseAnswers];
+        newChooseAnswers[quizIndex] = answerIndex;
+        setChooseAnswers(newChooseAnswers);
+    }
+
     return (
         <div>
-            <h1 className="text-3xl font-bold text-slate-950 mb-10 ">{quiz.question}</h1>
-            {quiz.answers.map((answer, index) => (
-                <h3
-                    key={index}
-                    className={`flex items-center text-2xl text-bold p-3 cursor-pointer   shadow-sm rounded-md m-2`}
-                >
-                    <div className="mr-4 w-6 h-6 p-[3px] border border-black rounded-full  ">
-                        {selectedIndex === index && (
-                            <div className="bg-slate-900 w-full h-full rounded-full"></div>
-                        )}
-                    </div>
-                    {answer}
-                </h3>
-            ))}
+            <h1 className="text-3xl font-bold text-slate-950 mb-10 ">
+                {quiz.question}
+            </h1>
+            {quiz.answers.map((answer, index) => {
+                const chosenAnswer = chooseAnswers[quizIndex];
+                let styleSelectedItem = "";
+                if (chosenAnswer == index) {
+                    styleSelectedItem =
+                        chosenAnswer == quiz.correctAnswer
+                            ? "bg-green-400"
+                            : "bg-rose-400";
+                }
+
+                return (
+                    <h3
+                        key={index}
+                        onClick={() => {
+                            if (chosenAnswer != null) return;
+                            updateChosenAnswer(quizIndex, index);
+                        }}
+                        className={
+                            "flex items-center text-2xl text-bold p-3 cursor-pointer shadow-sm rounded-md m-2 " +
+                            styleSelectedItem
+                        }
+                    >
+                        <div className="mr-4 w-6 h-6 p-[3px] border border-black rounded-full  ">
+                            {chosenAnswer == index && (
+                                <div className="bg-slate-900 w-full h-full rounded-full"></div>
+                            )}
+                        </div>
+                        {answer}
+                    </h3>
+                );
+            })}
             <div className="flex items-center justify-between mt-7">
                 {
                     <>
                         <button
-                            onClick={() => {
-                                setQuizIndex(quizIndex - 1);
-                                setSelector(-1);
-                            }}
+                            onClick={() => setQuizIndex(quizIndex - 1)}
                             disabled={quizIndex == 0}
                             className="disabled:bg-slate-300 px-6 py-2 rounded-md bg-slate-800 text-white text-xl"
                         >
                             Back
                         </button>
                         <button
-                            onClick={() => {
-                                setQuizIndex(quizIndex + 1);
-                                setSelector(-1);
-                            }}
-                            disabled={quizIndex == QUIZ_Data.length - 1}
+                            onClick={() => setQuizIndex(quizIndex + 1)}
+                            disabled={
+                                quizIndex == QUIZ_Data.length - 1 ||
+                                chooseAnswers[quizIndex]==null
+                            }
                             className="disabled:bg-slate-300 px-6 py-2 rounded-md bg-slate-800 text-white text-xl"
                         >
                             Next
